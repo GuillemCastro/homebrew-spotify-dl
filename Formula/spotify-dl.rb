99e6c641de90c8dd5af20df8e2d69a50a3f4197e
Guillem Castro <guillemcastro4@gmail.com>
2024-05-11 00:46:00 +0200

    fix styles

--- Formula/spotify-dl.rb
+++ Formula/spotify-dl.rb
@@ -1,15 +1,15 @@
 class SpotifyDl < Formula
-    desc "A command-line utility to download songs and playlists directly from Spotify's servers"
-    homepage "https://github.com/GuillemCastro/spotify-dl"
-    license "MIT"
-    url "https://github.com/GuillemCastro/spotify-dl.git", tag: "v0.2.1"
+  desc "Utility to download songs and playlists directly from Spotify's servers"
+  homepage "https://github.com/GuillemCastro/spotify-dl"
+  url "https://github.com/GuillemCastro/spotify-dl.git",
+    tag: "v0.2.1"
+  license "MIT"
 
-    depends_on "rust" => :build
-    depends_on "flac"
-    # On linux depends on alsa
-    depends_on "alsa-lib" if OS.linux?
+  depends_on "rust" => :build
+  depends_on "alsa-lib" if OS.linux?
+  depends_on "flac"
 
-    def install
-        system "cargo", "install", "--bin", "--git" , "https://github.com/GuillemCastro/spotify-dl.git", *std_cargo_args
-    end
+  def install
+    system "cargo", "install", "--bin", "--git", "https://github.com/GuillemCastro/spotify-dl.git", *std_cargo_args
+  end
 end
