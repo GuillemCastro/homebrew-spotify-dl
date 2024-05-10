a07929c03d2958b74e037b98eeb0b99af502828b
Guillem Castro <guillemcastro4@gmail.com>
2024-05-11 00:38:52 +0200

    build from source

--- Formula/spotify-dl.rb
+++ Formula/spotify-dl.rb
@@ -1,11 +1,14 @@
 class SpotifyDl < Formula
     desc "A command-line utility to download songs and playlists directly from Spotify's servers"
     homepage "https://github.com/GuillemCastro/spotify-dl"
-    url "https://github.com/GuillemCastro/spotify-dl/releases/download/v0.2.1/spotify-dl.macos.tar.gz"
-    sha256 "db2dd78ce7e4ec1e0a984eb3d55c46720884af291ed7b6d34fb1d21389c3ae63"
-    version "0.2.1"
+    license "MIT"
+
+    depends_on "rust" => :build
+    depends_on "flac"
+    # On linux depends on alsa
+    depends_on "alsa-lib" if OS.linux?
 
     def install
-        bin.install "spotify-dl"
+        system "cargo", "install", "--bin", "--git" , "https://github.com/GuillemCastro/spotify-dl.git", *std_cargo_args
     end
 end
