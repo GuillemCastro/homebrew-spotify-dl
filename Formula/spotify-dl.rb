f2bd96dfc9e6053c2fb803923a9b0700a6f54190
Guillem Castro <guillemcastro4@gmail.com>
2024-05-11 00:41:28 +0200

    add url

--- Formula/spotify-dl.rb
+++ Formula/spotify-dl.rb
@@ -2,6 +2,7 @@ class SpotifyDl < Formula
     desc "A command-line utility to download songs and playlists directly from Spotify's servers"
     homepage "https://github.com/GuillemCastro/spotify-dl"
     license "MIT"
+    url "https://github.com/GuillemCastro/spotify-dl.git", tag: "v0.2.1"
 
     depends_on "rust" => :build
     depends_on "flac"
