eb96645348ceb78b297e1b04d087dcaf6b725b77
Guillem Castro <GuillemCastro@users.noreply.github.com>
2025-07-06 21:27:50 +0000

    spotify-dl 0.8.0

--- Formula/spotify-dl.rb
+++ Formula/spotify-dl.rb
@@ -1,7 +1,7 @@
 class SpotifyDl < Formula
   desc "Utility to download songs and playlists directly from Spotify's servers"
   homepage "https://github.com/GuillemCastro/spotify-dl"
-  url "https://github.com/GuillemCastro/spotify-dl.git", tag: "v0.7.1", revision: "4b7df92fd56a0b0246814fe9782e6cfac1f4796a"
+  url "https://github.com/GuillemCastro/spotify-dl.git", tag: "v0.8.0", revision: "09fc850bcbbed6331c1c900c72cd629f0d34ea55"
   license "MIT"
 
   depends_on "cmake" => :build
