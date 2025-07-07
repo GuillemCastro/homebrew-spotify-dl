bf469b65a15e73620677101bd337ed0ab7985c54
Guillem Castro <guillemcastro4@gmail.com>
2025-07-07 23:28:18 +0200

    Update spotify-dl.rb

--- Formula/spotify-dl.rb
+++ Formula/spotify-dl.rb
@@ -4,10 +4,8 @@ class SpotifyDl < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/GuillemCastro/spotify-dl/releases/download/v0.8.0/spotify-dl.macos-aarch64"
-      sha256 "d4f28336a91fa633699982e019f5e7a67025ae19ba4e6ab660cb74689c3e1231"
-    end
+    url "https://github.com/GuillemCastro/spotify-dl/releases/download/v0.8.0/spotify-dl.macos-aarch64"
+    sha256 "d4f28336a91fa633699982e019f5e7a67025ae19ba4e6ab660cb74689c3e1231"
   end
 
   on_linux do
