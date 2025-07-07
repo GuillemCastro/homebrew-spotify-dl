7d23bb5be4fd37e71b60c750d160d32cdf353c81
Guillem Castro <GuillemCastro@users.noreply.github.com>
2025-07-07 21:32:35 +0000

    spotify-dl 0.9.0

--- Formula/spotify-dl.rb
+++ Formula/spotify-dl.rb
@@ -3,8 +3,8 @@ class SpotifyDl < Formula
   homepage "https://github.com/GuillemCastro/spotify-dl"
   license "MIT"
 
-  url "https://github.com/GuillemCastro/spotify-dl/releases/download/v0.8.0/spotify-dl.macos-aarch64"
-  sha256 "d4f28336a91fa633699982e019f5e7a67025ae19ba4e6ab660cb74689c3e1231"
+  url "https://github.com/GuillemCastro/spotify-dl/releases/download/v0.9.0/spotify-dl.macos-aarch64"
+  sha256 "b71de6f91c5ef7fbd83d0a2c7d6863da1ef4dbace20bc676893467c3fc0a3905"
 
   def install
     bin.install "#{cached_download}" => "spotify-dl"
